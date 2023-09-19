50b12a59f4105c450b68309536ec643e6a45fe30
Christian Nicolai Winkler <101516854+chrwinkler@users.noreply.github.com>
2023-09-19 08:49:05 +0200

    Add files via upload

---
+++ Assignment 3_4/Exercise 1/Exercise_1.cpp
@@ -0,0 +1,74 @@
+#include "Week_03/stack_class.h"
+#include "Exe1.h"
+#include <isostream>
+
+class Array_constructor(a[],) {
+    publuc;
+    New_Array( )
+
+    default = a[Null];
+    if(default < NuLL) {
+
+    }
+
+
+}
+
+
+template <typename Object>
+
+template <typename Stack_Size>
+
+int size = 100;
+int Current_Stack = a[size];
+int Row_Numb = 0;
+
+
+int push(x)
+for(int i = 0; i < size; i++) {
+
+    if(Row_Numb < size){
+        push(x);
+        Row_Numb ++
+        std::cout << " Strack is full!, Creat a new One with Double"
+    else {
+        push(x)
+    }
+    }
+}
+// Creating the new array that will have the double the size 
+int Double() {
+    
+    New_Stack = new Object[size*2]
+    int i = 0
+    for(int i = 0; i < size; i++){
+            push(a[i]) // Pushing the olf value 
+        }
+}
+
+
+    if(i =< size){
+    push(Object);
+    }
+    else{
+    size = size * 2
+    }
+}
+
+
+
+
+// To-Do 
+// - Implement a Stack using an ordinary array
+// - The stack should be initialized with a size 
+//   parameter that defines the initial size of the stack
+// - The default constructor should create a stack with 
+//   100 elements as the initial size.
+// - When push(...) is called and the stack is full.
+// - a new array should be allocated that have the double size,
+//      - the content of the old array copied to the new one 
+//        and the old array deleted. 
+// - Analyze the running time of the stack operations push 
+//   and pop in terms of the N elements it stores.
+
+
