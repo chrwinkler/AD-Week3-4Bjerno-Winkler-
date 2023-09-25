f23d81d683c9ddf4df49ae9d256c63f6f2151e18
hipdoghotdog <114659437+hipdoghotdog@users.noreply.github.com>
2023-09-25 15:50:33 +0200

    search test

--- Assignment 3_4/Exercise 3/matrix_search.cpp
+++ Assignment 3_4/Exercise 3/matrix_search.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "stdlib.h"
-int search(int mat[][], int n, int x) // mat = matrix, n is number of rows or collumns, x is the value we are searcing for
+int search(int mat[][2], int n, int x) // mat = matrix, n is number of rows or collumns, x is the value we are searcing for
 {
     if (n == 0) // If the matrix is of size zero we exit
         return -1;
@@ -27,5 +27,25 @@ int search(int mat[][], int n, int x) // mat = matrix, n is number of rows or co
     }
 
     std::cout << "n Element not found" << std::endl;
+    return 0;
+}
+
+int main()
+{
+    int n = 2;
+    int mat[2][2];
+    int f = 2;
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            mat[i][j] = f;
+            f++;
+        }
+    }
+    search(mat, n, 10);
+    search(mat, n, 2);
+    search(mat, n, 5);
+
     return 0;
 }
